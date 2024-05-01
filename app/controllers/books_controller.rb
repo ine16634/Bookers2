1cbf076bf4afe717d003e7a9bc9ece403fb007a2
ine16634 <62016478+ine16634@users.noreply.github.com>
2024-05-01 10:50:19 +0000

    [Finish] bookの編集完成のデストロイ完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -31,6 +31,11 @@ class BooksController < ApplicationController
     end
   end
 
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+  end
 
 
  private

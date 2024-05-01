2d939980c26e075f42bfd28e4776b235e77a6469
ine16634 <62016478+ine16634@users.noreply.github.com>
2024-05-01 14:07:30 +0000

    [Finish] userのeditの制限まで実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,41 @@
 class UsersController < ApplicationController
-  
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
   end
 
   def edit
-  
+    
+   @user = User.find(params[:id])
   end
-  
+
   def update
- 
+   @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+       render :edit
+    end
   end
 
   def index
    @users = User.all
   end
-  
 
-  
-  
+
+
+  private
+  def is_matching_login_user
+      user = User.find(params[:id])
+   unless user.id == current_user.id
+    redirect_to user_path(current_user.id)
+   end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image,:profile)
+  end
+
 end

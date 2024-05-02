4d59d544bd3e6f87e517ab17e98655123a4a85ef
ine16634 <62016478+ine16634@users.noreply.github.com>
2024-05-02 17:13:54 +0000

    [Finish]新規作成のエラー表示まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,23 +18,25 @@ class BooksController < ApplicationController
   end
 
   def new
-   
+
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      flash[:notice] = "You have created book successfully.。"
       redirect_to @book
     else
-      render :new
+      @books = Book.all
+      render :index
     end
   end
 
   def destroy
-    book = Book.find(params[:id])  
-    book.destroy  
-    redirect_to '/books'  
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
 

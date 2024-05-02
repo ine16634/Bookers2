2621f0b07db3d1db5c3ca954571242f8955104d9
ine16634 <62016478+ine16634@users.noreply.github.com>
2024-05-02 15:41:02 +0000

    [Finish]カラムを変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   validates :name, presence: true 
-  validates :profile, length: { maximum: 50 } 
+  validates :introduction, length: { maximum: 50 } 
          
   has_many :books, dependent: :destroy  
   
---
+++ db/migrate/20240502152947_rename_opinion_column_to_books.rb
@@ -0,0 +1,6 @@
+class RenameOpinionColumnToBooks < ActiveRecord::Migration[6.1]
+
+  def change
+    rename_column :books, :opinion, :body
+  end
+end

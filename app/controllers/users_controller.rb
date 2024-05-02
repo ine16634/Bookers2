8d2fd45f1a5364027f47a526cc15044c1d8fe78d
ine16634 <62016478+ine16634@users.noreply.github.com>
2024-05-02 16:28:38 +0000

    [Finish]カラムの修正しきれてなかったところを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   end
 
   def update
-   @user = User.find(params[:id])
+    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user.id)
     else
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :profile_image,:profile)
+    params.require(:user).permit(:name, :profile_image,:introduction)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  validates :name, presence: true 
-  validates :introduction, length: { maximum: 50 } 
-         
-  has_many :books, dependent: :destroy  
-  
+
+  validates :name, presence: true , length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
+
+  has_many :books, dependent: :destroy
+
   has_one_attached :profile_image
-  
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
@@ -18,5 +18,5 @@ class User < ApplicationRecord
     end
       profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
 end

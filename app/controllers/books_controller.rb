4e6085fa7f471b2b42673f25d5e463768e9edde8
ine16634 <62016478+ine16634@users.noreply.github.com>
2024-05-03 06:10:55 +0000

    [Finish]Bootstrapでレイアウトを整えた

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "You have created book successfully."
     redirect_to book_path(book.id)
   end
 
@@ -25,7 +26,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      flash[:notice] = "You have created book successfully.。"
+      flash[:notice] = "You have created book successfully."
       redirect_to @book
     else
       @books = Book.all
